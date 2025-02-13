121351806a8ebee62d775665cab66630fe4f8141
taejin7088 <gog70888@gmail.com>
2025-02-13 09:31:59 +0900

    [part2]02.배열 group-by 완료

--- part2/02.배열/(lv.4)group-by.js
+++ part2/02.배열/(lv.4)group-by.js
@@ -28,7 +28,13 @@
  */
 
 // TODO: groupBy 함수를 작성하세요.
-function groupBy(arr, key) {}
+function groupBy(arr, key) {
+  return arr.reduce((acc, el) => {
+    if (!acc[el[key]]) acc[el[key]] = [];
+    acc[el[key]].push(el);
+    return acc;
+  }, {});
+}
 
 // export를 수정하지 마세요.
 export { groupBy };
